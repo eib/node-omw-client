9a81c084c397f2d7bdc8d35f5b91081cf751d78e
EthanB <ethan@eblackwelder.com>
2015-06-18 22:26:48 -0400

    Implementing music-player.
    Minimally multi-browser-compatible.

--- index.js
+++ index.js
@@ -1,17 +1,51 @@
-var io = require('socket.io-client'),
-  url = process.env.REMOTE_URL || 'http://localhost:8001',
-  socket = io.connect(url),
-  name = process.env.SERVER_NAME || 'omw-client',
-  items = [
-      { name: 'a', id: 1 },
-      { name: 'b', id: 2 },
-      { name: 'c', id: 3 },
-    ];
+var path = require('path'),
+  glob = require('glob'),
+  slash = require('slash'),
+  io = require('socket.io-client'),
+  os = require("os"),
+  player = require('./lib/player'),
+  url = (process.env.REMOTE_URL || 'http://localhost:8001').trim(),
+  name = (process.env.SERVER_NAME || os.hostname()).trim(),
+  musicDir = (process.env.MUSIC_DIR || (__dirname + '/music')).trim(),
+  socket = io.connect(url);
 
-function register() {
+function findFiles(pattern, workingDir) {
+  console.log('Pattern: ', pattern);
+  console.log('Dir: "' + workingDir + '"');
+  return glob.sync(pattern, { cwd: workingDir });
+}
+
+function findMusicFiles() {
+  return findFiles('**/*.mp3', slash(musicDir));
+}
+
+function mapFilesToItems(files) {
+  console.log('Files:', files);
+  return files.map(function (file) {
+    return {
+      id: file,
+      name: path.basename(file, path.extname(file)),
+    };
+  });
+}
+
+function registerItems(items) {
   socket.emit('register', { name: name, items: items }, function (response) {
     console.log('Sent items:', JSON.stringify(response));
   });
+}
+
+function register() {
+  return Promise.resolve()
+    .then(findMusicFiles)
+    .then(mapFilesToItems)
+    .then(registerItems);
+}
+
+function playItem(item) {
+  var filePath = path.join(musicDir, item.id);
+  console.log('Playing ' + filePath);
+  player.play(filePath);
 }
 
 console.log('Opening ', url);
@@ -23,6 +57,7 @@ socket.on('connect', function () {
 
 socket.on('selectItem', function (data) {
   console.log('Selected:', JSON.stringify(data));
+  playItem(data.item);
 });
 
 socket.on('error', function (err) {
